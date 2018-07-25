a983885a2a7cd4b12f7e6ffeb10e0ad969da2f2a
zEttOn86 <38281934+zEttOn86@users.noreply.github.com>
2018-07-25 15:30:06 +0900

    Change markdown

--- markdown2pdf.bat
+++ markdown2pdf.bat
@@ -2,9 +2,13 @@
 
 ::::::::::Parameter setting::::::::::
 SET ROOT=%~dp0
-SET HEADER=%ROOT%headers\h-luatexja.tex
 SET INPUT_DIR=%ROOT%posts\
-SET OUTPUT_DIR=%ROOT%pdf\
+SET OUTPUT_PDF_DIR=%ROOT%pdf\
+SET OUTPUT_HTML_DIR=%ROOT%html\
+SET STYLE_CSS=%ROOT%css\style.css
+SET FONT_CSS=%ROOT%css\import-font.css
+SET DYNOLOAD=%ROOT%js\dynoload.js
+SET CHROME="%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe"
 
 :: Get latest file name
 :: https://code.i-harness.com/ja/q/17c5b
@@ -20,7 +24,11 @@ EXIT
 
 :::::::::: RUN ::::::::::
 :SUB
-ECHO Start to generate " %~n1.pdf "
-CALL pandoc %INPUT_DIR%%~1 -o %OUTPUT_DIR%%~n1.pdf --pdf-engine=lualatex -H %HEADER%
+
+ECHO Start to generate " %~n1.html and %~n1.pdf "
+CALL pandoc --self-contained -s --mathjax=%DYNOLOAD% %INPUT_DIR%%~1 -o %OUTPUT_HTML_DIR%%~n1.html -c %STYLE_CSS% -H %FONT_CSS%
+ECHO Convert html to pdf using Google chrome
+%CHROME% --headless --print-to-pdf=%OUTPUT_PDF_DIR%%~n1.pdf %OUTPUT_HTML_DIR%%~n1.html
 ECHO Done
+
 EXIT /B
